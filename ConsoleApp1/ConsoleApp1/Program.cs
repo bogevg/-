72ab1019cc2391fd4b5fa838cc53537b8ee78612
89110868071@mai.ru <bogevg>
2024-01-20 17:22:14 +0300

    С программой

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿string s;
+int n = 0;
+Console.WriteLine("Введите слова");
+s = Console.ReadLine();
+string[] ar_word = s.Split(' ');
+for (int i = 0; i < ar_word.Length; i++)
+{
+   if (ar_word[i].Length <= 3) {
+        n++;
+    } 
+}
+string[] ar_new = new string[n];
+int l = n;
+for (int i = 0; i < ar_word.Length; i++)
+{
+    if (ar_word[i].Length < 4)
+    {
+        ar_new[l - n] = ar_word[i];
+        n--;
+    }
+}
